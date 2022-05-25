0ca5ab3554510d67993fa92900820a390873b582
i-am-vaibhav <vg566556@gmail.com>
2022-05-26 02:08:13 +0530

    initial commit

---
+++ src/main/java/com/anvl/recipe/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.anvl.recipe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"/","","index","index.html"})
+    public String indexPage(){
+        return "index";
+    }
+
+}
