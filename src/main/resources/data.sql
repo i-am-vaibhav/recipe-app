abfa84112982f54aa06a417905806a425fed2702
i-am-vaibhav <vg566556@gmail.com>
2022-06-02 19:13:29 +0530

    added datadump

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO CATEGORY (id,description) VALUES (1000,'American');
+INSERT INTO CATEGORY (id,description) VALUES (1001,'Indian');
+INSERT INTO CATEGORY (id,description) VALUES (1002,'Mexican');
+INSERT INTO CATEGORY (id,description) VALUES (1003,'Fast Food');
+INSERT INTO UNIT_OF_MEASURE (id,uom) VALUES (1004,'Teaspoon');
+INSERT INTO UNIT_OF_MEASURE (id,uom) VALUES (1005,'Tablespoon');
+INSERT INTO UNIT_OF_MEASURE (id,uom) VALUES (1006,'Cup');
+INSERT INTO UNIT_OF_MEASURE (id,uom) VALUES (1007,'Pinch');
+INSERT INTO UNIT_OF_MEASURE (id,uom) VALUES (1008,'Ounce');
